f789011f626d0a00741dc7b949fee18817922359
Yordan Georgiev <yordan.georgiev@gmail.com>
2021-07-09 13:24:55 +0000

    feat: add capability to execute control flow on alpine Linux in run.sh

--- src/bash/run/run.sh
+++ src/bash/run/run.sh
@@ -4,7 +4,14 @@ unit_run_dir=$(perl -e 'use File::Basename; use Cwd "abs_path"; print dirname(ab
 export PRODUCT_DIR=$(cd $unit_run_dir/../../.. ; echo `pwd`)
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	bash $PRODUCT_DIR/src/bash/run/ubuntu/run.sh "$@"
+    distro=$(cat /etc/os-release|egrep '^ID='|cut -d= -f2)
+    if [ $distro == "ubuntu" ]; then
+        bash $PRODUCT_DIR/src/bash/run/ubuntu/run.sh "$@"
+    elif [ $distro == "alpine" ]; then
+        bash $PRODUCT_DIR/src/bash/run/alpine/run.sh "$@"
+    else
+       echo "your Linux distro is not supported !!!"
+    fi
 elif [ "$(expr substr $(uname -s) 1 5)" != "Linux" ]; then
    echo "your OS is not supported !!!"
 fi
